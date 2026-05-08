8bc2bee800ea29763522a783bee35324f10db56c
Michael <150398125+mprob79@users.noreply.github.com>
2026-05-08 09:59:39 -0700

    Rename SplashForm to frmMain and update methods

--- frmMain.cs
+++ frmMain.cs
@@ -1,11 +1,13 @@
+// rename class and constructor to frmMain - Mike
+
 using System;                                      // using System namespace
 using System.Windows.Forms;                        // using Windows Forms namespace
 
 namespace DeliciosoERistorante                     // project namespace
 {
-    public partial class SplashForm : Form         // SplashForm class
+    public partial class frmMain : Form          // frmMain class
     {
-        public SplashForm()                        // constructor
+        public frmMain()                         // constructor
         {
             InitializeComponent();                 // initialize form components
             MaximizeSplashForm();                  // call method to maximize form
@@ -16,14 +18,14 @@ namespace DeliciosoERistorante                     // project namespace
             this.WindowState = FormWindowState.Maximized;   // set window state to maximized
         }
 
-        private void btnEnter_Click(object sender, EventArgs e)   // enter button click event
+        private void btnMenu_Click(object sender, EventArgs e)   // menu button click event
         {
-            OpenfrmMenu();                        // call method to open menu form
+            OpenfrmMenu();                         // call method to open menu form
         }
 
-        private void OpenfrmMenu()                // method to open menu form
+        private void OpenfrmMenu()                 // method to open menu form
         {
-            frmMenu menu = new frmMenu();        // create new instance of frmMenu
+            frmMenu menu = new frmMenu();          // create new instance of frmMenu
             menu.Show();                           // show menu form
             this.Hide();                           // hide splash form
         }
