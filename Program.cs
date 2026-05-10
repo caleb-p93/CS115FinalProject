66ec926dca5262fa0da068e664de317629869a51
Michael <150398125+mprob79@users.noreply.github.com>
2026-05-10 00:15:30 -0700

    Final with notes

--- Program.cs
+++ Program.cs
@@ -1,10 +1,18 @@
+/*
+Group Name: 
+Students Names: Michael Roberts, Caleb Perkins, Andy G Mendoza
+Final Project
+Description:  This is a digital menu.  Customers can interact with it.  It will take them through an ordering and reordering process.  Then to payment, and finally a receipt page where they can save their
+receipt.  The program is designed to automatically reset once a customer is done.  Each customer will receive a unique number that will display on the saved txt receipt.
+*/
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace DeliciosoERistorante // changed from namespace FinalProect 5.8.26 - Mike
+namespace DeliciosoERistorante
 {
     internal static class Program
     {
@@ -16,7 +24,7 @@ namespace DeliciosoERistorante // changed from namespace FinalProect 5.8.26 - Mi
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMain()); 
+            Application.Run(new frmMain());
         }
     }
 }
