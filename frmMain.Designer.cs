133bb64f0ed18fb842546dd6dba3d1b06fdf9acc
Michael <150398125+mprob79@users.noreply.github.com>
2026-05-10 00:23:50 -0700

    Final

--- frmMain.Designer.cs
+++ frmMain.Designer.cs
@@ -1,5 +1,3 @@
-// update class to frmMain - Mike
-
 namespace DeliciosoERistorante
 {
     partial class frmMain
@@ -20,131 +18,67 @@ namespace DeliciosoERistorante
 
         private void InitializeComponent()                             // initialize components
         {
-            System.ComponentModel.ComponentResourceManager resources =
-                new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
-
-            this.btnMenu = new System.Windows.Forms.Button();          // menu button
-            this.lblSplashTitle = new System.Windows.Forms.Label();    // splash title label
-            this.lblSplashWelcome = new System.Windows.Forms.Label();  // welcome label
-
-            this.SuspendLayout();                                      // suspend layout
-
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnMenu = new System.Windows.Forms.Button();
+            this.lblSplashTitle = new System.Windows.Forms.Label();
+            this.lblSplashWelcome = new System.Windows.Forms.Label();
+            this.SuspendLayout();
             // 
             // btnMenu
             // 
-            this.btnMenu.BackColor = System.Drawing.Color.Wheat;       // background color
-
-            this.btnMenu.Font = new System.Drawing.Font(
-                "Microsoft Sans Serif",
-                15.75F,
-                System.Drawing.FontStyle.Bold,
-                System.Drawing.GraphicsUnit.Point,
-                ((byte)(0)));
-
-            this.btnMenu.Location = new System.Drawing.Point(700, 153); // location
-            this.btnMenu.Margin = new System.Windows.Forms.Padding(4); // margin
-            this.btnMenu.Name = "btnMenu";                             // name
-            this.btnMenu.Size = new System.Drawing.Size(181, 69);      // size
-            this.btnMenu.TabIndex = 0;                                 // tab index
-            this.btnMenu.Text = "MENU";                                // text
-            this.btnMenu.UseVisualStyleBackColor = false;              // use style
-
-            this.btnMenu.Click +=
-                new System.EventHandler(this.btnMenu_Click);           // event hookup
-
+            this.btnMenu.BackColor = System.Drawing.Color.Wheat;
+            this.btnMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMenu.Location = new System.Drawing.Point(1151, 255);
+            this.btnMenu.Margin = new System.Windows.Forms.Padding(4);
+            this.btnMenu.Name = "btnMenu";
+            this.btnMenu.Size = new System.Drawing.Size(181, 69);
+            this.btnMenu.TabIndex = 0;
+            this.btnMenu.Text = "MENU";
+            this.btnMenu.UseVisualStyleBackColor = false;
+            this.btnMenu.Click += new System.EventHandler(this.btnMenu_Click);
             // 
             // lblSplashTitle
             // 
-            this.lblSplashTitle.AutoSize = true;                       // autosize
+            this.lblSplashTitle.AutoSize = true;
             this.lblSplashTitle.BackColor = System.Drawing.Color.OldLace;
-
-            this.lblSplashTitle.Font = new System.Drawing.Font(
-                "Palatino Linotype",
-                24F,
-                System.Drawing.FontStyle.Bold,
-                System.Drawing.GraphicsUnit.Point,
-                ((byte)(0)));
-
-            this.lblSplashTitle.Location = new System.Drawing.Point(617, 30);
+            this.lblSplashTitle.Font = new System.Drawing.Font("Palatino Linotype", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSplashTitle.Location = new System.Drawing.Point(1063, 143);
             this.lblSplashTitle.Name = "lblSplashTitle";
             this.lblSplashTitle.Size = new System.Drawing.Size(345, 44);
             this.lblSplashTitle.TabIndex = 4;
             this.lblSplashTitle.Text = "Delicioso E-Ristorante";
-
             // 
             // lblSplashWelcome
             // 
             this.lblSplashWelcome.AutoSize = true;
-
-            this.lblSplashWelcome.BackColor =
-                System.Drawing.SystemColors.ControlLightLight;
-
-            this.lblSplashWelcome.Font = new System.Drawing.Font(
-                "Palatino Linotype",
-                18F,
-                System.Drawing.FontStyle.Bold,
-                System.Drawing.GraphicsUnit.Point,
-                ((byte)(0)));
-
-            this.lblSplashWelcome.Location =
-                new System.Drawing.Point(573, 317);
-
+            this.lblSplashWelcome.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblSplashWelcome.Font = new System.Drawing.Font("Palatino Linotype", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSplashWelcome.Location = new System.Drawing.Point(1042, 402);
             this.lblSplashWelcome.Name = "lblSplashWelcome";
-
-            this.lblSplashWelcome.Size =
-                new System.Drawing.Size(418, 32);
-
+            this.lblSplashWelcome.Size = new System.Drawing.Size(418, 32);
             this.lblSplashWelcome.TabIndex = 5;
-
-            this.lblSplashWelcome.Text =
-                "Welcome - Please tap MENU to begin";
-
+            this.lblSplashWelcome.Text = "Welcome - Please tap MENU to begin";
             // 
             // frmMain
             // 
-            this.AutoScaleDimensions =
-                new System.Drawing.SizeF(7F, 16F);
-
-            this.AutoScaleMode =
-                System.Windows.Forms.AutoScaleMode.Font;
-
-            this.BackColor =
-                System.Drawing.SystemColors.ActiveCaption;
-
-            this.BackgroundImage =
-                ((System.Drawing.Image)
-                (resources.GetObject("$this.BackgroundImage")));
-
-            this.BackgroundImageLayout =
-                System.Windows.Forms.ImageLayout.Stretch;
-
-            this.ClientSize =
-                new System.Drawing.Size(1284, 749);
-
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1920, 1080);
             this.Controls.Add(this.lblSplashWelcome);
             this.Controls.Add(this.lblSplashTitle);
             this.Controls.Add(this.btnMenu);
-
             this.DoubleBuffered = true;
-
-            this.Font = new System.Drawing.Font(
-                "Palatino Linotype",
-                8.25F,
-                System.Drawing.FontStyle.Bold,
-                System.Drawing.GraphicsUnit.Point,
-                ((byte)(0)));
-
-            this.FormBorderStyle =
-                System.Windows.Forms.FormBorderStyle.None;
-
-            this.Margin =
-                new System.Windows.Forms.Padding(4);
-
+            this.Font = new System.Drawing.Font("Palatino Linotype", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmMain";
             this.Text = "Splash Page";
-
             this.ResumeLayout(false);
             this.PerformLayout();
+
         }
 
         #endregion
